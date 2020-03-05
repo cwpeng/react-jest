b5acc40078a9a4bca3fe3067be2a6ec91bfbc8a2
cwpeng <padalab@gmail.com>
2020-03-05 22:26:56 +0800

    Init Project

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+class App extends React.Component{
+	constructor(props){
+		super(props);
+	}
+	render(){
+		return <>
+			<h1>Unit Testing</h1>
+			<main>Jest Snapshot Testing</main>
+		</>;
+	}
+}
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path=require("path");
+module.exports={
+    mode: "development",
+    entry:{
+		index: "./src/index.js"
+	},
+    output:{
+        path: path.resolve(__dirname, "dist"),
+        filename: "main.js"
+    },
+    module:{
+        rules:[
+			{ test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+		]
+    }
+};
